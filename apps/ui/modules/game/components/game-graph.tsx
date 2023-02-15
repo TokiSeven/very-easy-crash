06bfde9016f5872ef4588448fdb8dd2719604c6c
Владимир Рябцев (TokiSeven - MacPro) <tokiseven3@gmail.com>
2023-02-15 07:05:01 +0500

    finish chart

--- apps/ui/modules/game/components/game-graph.tsx
+++ apps/ui/modules/game/components/game-graph.tsx
@@ -1,11 +1,44 @@
-import { FC } from 'react';
+import { FC, useEffect } from 'react';
 import styled from 'styled-components';
 import { useAppSelector } from '../store';
 
+let offsetX = 0;
+let isFirstTouch = true;
+
 export const GameGraph: FC = () => {
-  const { state, secretNumber, rate } = useAppSelector((s) => s.gameState);
+  const { rate } = useAppSelector((s) => s.gameState);
+  useEffect(() => {
+    const canvas = document.getElementById('game') as HTMLCanvasElement;
+    if (canvas) {
+      const ctx = canvas.getContext('2d');
+      if (ctx) {
+        if (isFirstTouch) {
+          ctx.beginPath();
+          ctx.moveTo(0, canvas.height);
+          isFirstTouch = false;
+        }
+        if (!rate) {
+          ctx.restore();
+          ctx.moveTo(0, canvas.height);
+          offsetX = 0;
+          isFirstTouch = true;
+        } else {
+          ctx.lineTo(offsetX, -rate * (canvas.height / 10) + canvas.height);
+          ctx.stroke();
+          offsetX += canvas.width / 35;
+        }
+      } else {
+        isFirstTouch = true;
+        offsetX = 0;
+      }
+    } else {
+      isFirstTouch = true;
+      offsetX = 0;
+    }
+  }, [rate]);
   return (
     <Layout>
+      {!!rate && <Canvas id="game"></Canvas>}
       <Rate>{(rate || 0).toFixed(2)}x</Rate>
     </Layout>
   );
@@ -17,6 +50,15 @@ const Layout = styled.div`
   position: relative;
   justify-content: center;
   align-items: center;
+  height: 300px;
 `;
 
-const Rate = styled.div``;
+const Rate = styled.div`
+  position: absolute;
+  text-align: center;
+`;
+
+const Canvas = styled.canvas`
+  height: 100%;
+  width: 100%;
+`;
